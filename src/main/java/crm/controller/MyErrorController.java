d5b7d9e8c2c716959b5b39e16d1528101d776f11
Modernize Studio <studio@modernize.com>
2026-09-04 14:26:01 +0000

    Commit for planId: 108

--- src/main/java/crm/controller/MyErrorController.java
+++ src/main/java/crm/controller/MyErrorController.java
@@ -1,22 +1,22 @@
 package crm.controller;
 
-import org.springframework.boot.autoconfigure.web.ErrorController;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@RestController
+@Controller
 public class MyErrorController implements ErrorController {
 
-    private static final String PATH = "/error";
-
-    @RequestMapping(value = PATH)
-    public String error() {
-        return "Error handling";
-    }
-
-    @Override
-    public String getErrorPath() {
-        return PATH;
+    @RequestMapping("/error")
+    @ResponseBody
+    public String error(HttpServletRequest request) {
+        Integer statusCode = (Integer) request.getAttribute("jakarta.servlet.error.status_code");
+        Exception exception = (Exception) request.getAttribute("jakarta.servlet.error.exception");
+        return String.format("<html><body><h2>Error Page</h2><div>Status code: <b>%s</b></div>"
+                + "<div>Exception Message: <b>%s</b></div><body></html>",
+                statusCode, exception == null ? "N/A" : exception.getMessage());
     }
 
 }
